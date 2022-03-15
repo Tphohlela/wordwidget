c4e49dc3ccf779b2b0abbcc53de1fb9a5d46b73c
Tphohlela <17thapho@gmail.com>
2022-03-15 19:48:34 +0200

    style and error message

--- word.js
+++ word.js
@@ -7,19 +7,14 @@ let wordsMoreThanFourStr = '';
 
 const wordGame = () => {
     const sentence = textElem.value
-
     const newArray = sentence.split(' ');
-    console.log('check' + newArray[4])
 
-    //if(newArray[4]){
     if (newArray[4] == undefined) {
         errorElem.innerHTML = 'Please enter a sentence that has more than 5 words'
         setTimeout(() => errorElem.innerHTML = "", 5000);
-        displaySentence.innerHTML = ` `;
-        displaySentenceLength = ` `;
     }
 
-    else {
+    else if (newArray[4] != undefined) {
         newArray.forEach(element => {
             if (element.length > 4) {
                 wordsMoreThanFourStr += `<mark>${element}</mark> `
@@ -28,12 +23,10 @@ const wordGame = () => {
                 wordsMoreThanFourStr += `${element} `
             }
         });
-    }
 
-    if (sentence) {
         const lengthOfSentence = sentence.split(" ").length;
         displaySentenceLength.innerHTML = `Number of words in a sentence : ${lengthOfSentence}`
-    }
 
-    displaySentence.innerHTML = `Words that have more than 4 characters : ${wordsMoreThanFourStr}`
+        displaySentence.innerHTML = `Words that have more than 4 characters : ${wordsMoreThanFourStr}`
+    }
 }
