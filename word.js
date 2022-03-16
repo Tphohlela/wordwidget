c90f8d4334f0f92282715e520433a0354820186d
Tphohlela <17thapho@gmail.com>
2022-03-16 12:24:22 +0200

    fixing duplicate issue

--- word.js
+++ word.js
@@ -3,12 +3,25 @@ const displaySentenceLength = document.querySelector('.sentenceLength')
 const displaySentence = document.querySelector('.wordSentence')
 const highlightedWords = document.querySelector('.highlight')
 const errorElem = document.querySelector('.error')
-let wordsMoreThanFourStr = '';
+const checkBox = document.querySelector('.checkbox')
+
+
+
+if(checkBox.checked == true ){
+
+    displaySentence.innerHTML = `tt is a city girl`
+}
 
 const wordGame = () => {
+
+    let wordsMoreThanFourStr = '';
+    
     const sentence = textElem.value
     const newArray = sentence.split(' ');
+    // console.log(checkBox.checked)
 
+    textElem.value = null;
+    
     if (newArray[4] == undefined) {
         errorElem.innerHTML = 'Please enter a sentence that has more than 5 words'
         setTimeout(() => errorElem.innerHTML = "", 5000);
@@ -28,5 +41,12 @@ const wordGame = () => {
         displaySentenceLength.innerHTML = `Number of words in a sentence : ${lengthOfSentence}`
 
         displaySentence.innerHTML = `Words that have more than 4 characters : ${wordsMoreThanFourStr}`
+    }
+}
+
+const hideAndHighlight = () => {
+
+    if(checkBox.checked == true){
+        displaySentence.innerHTML = `Hey`   
     }
 }
